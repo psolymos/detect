b809d82344a8b42e9c9d2c4327f1df7e590707e0
Peter Solymos <psolymos@gmail.com>
2015-06-03 10:50:17 -0600

    NB fun added

--- R/svabu.R
+++ R/svabu.R
@@ -2,7 +2,7 @@ svabu <-
 function (formula, data, zeroinfl=TRUE, area=1, N.max=NULL, inits,
     link.det = "logit", link.zif = "logit",
     model = TRUE, x = FALSE, 
-#    distr = c("P", "NB"), 
+    distr = c("P", "NB"), 
     ...)
 {
 #    distr <- match.arg(distr)
@@ -59,17 +59,17 @@ function (formula, data, zeroinfl=TRUE, area=1, N.max=NULL, inits,
 #return(list(Y=Y,X=X,Z=Z,Q=Q))
 
     ## fit
-#    if (distr == "P") {
+    if (distr == "P") {
         fit <- svabu.fit(Y, X, Z, Q, zeroinfl=zeroinfl, area, N.max, inits, 
             link.det, link.zif, ...)
         sclass <- "svabu_p"
-#    }
-#    if (distr == "NB") {
-#        stop("NB not yet implemented")
-#        fit <- svabu_nb.fit(Y, X, Z, Q, zeroinfl=zeroinfl, area, N.max, inits, 
-#            link.det, link.zif, ...)
-#        sclass <- "svabu_nb"
-#    }
+    }
+    if (distr == "NB") {
+        stop("NB not yet implemented")
+        fit <- svabu_nb.fit(Y, X, Z, Q, zeroinfl=zeroinfl, area, N.max, inits, 
+            link.det, link.zif, ...)
+        sclass <- "svabu_nb"
+    }
     ## return value
     out <- c(fit, out)
     if (!model) 
