5cda62d0fb7df417da7b6939bf86a9e3db9d2362
Peter Solymos <psolymos@gmail.com>
2016-11-21 12:24:59 -0700

    predict method

---
+++ R/predict.cmulti.R
@@ -0,0 +1,19 @@
+predict.cmulti <-
+function(object, newdata = NULL, type = c("link", "response"), ...)
+{
+    if (object$type == "mix")
+        stop("fitted values are not available for finite mixture model")
+    if (is.null(newdata)) {
+        rval <- fitted(object)
+        ## fitted gives 'response', need to use linkfun for 'link'
+        if (type == "link")
+            rval <- poisson("log")$linkfun(rval)
+    } else {
+        X <- model.matrix(formula(object$formula, lhs=c(FALSE, FALSE)), newdata)
+        rval <- drop(X %*% coef(object))
+        if (type == "response") {
+            rval <- poisson("log")$linkinv(rval)
+        }
+    }
+    rval
+}
