2479e1b2953703fc27ac01a1c7df8750d31b11d4
Peter Solymos <psolymos@gmail.com>
2018-01-02 12:47:49 -0700

    fit0 adjusted

--- R/cmulti.fit0.R
+++ R/cmulti.fit0.R
@@ -1,18 +1,20 @@
 ## constant conditional multinomial model
 ## collapsing counts by methodology
 cmulti.fit0 <-
-function(Y, D, type=c("rem", "mix", "dis"), interval=c(-25, 25), ...)
+function(Y, D, type=c("rem", "mix", "dis", "fmix"), interval=c(-25, 25), ...)
 {
     type <- match.arg(type)
     pifun <- switch(type,
             "dis" = function(r, tau) 1-exp(-(r/tau)^2),
             "rem"  = function(t, phi) 1-exp(-t*phi),
-            "mix"  = function(t, phi, c) 1-c*exp(-t*phi))
-    nll.fun <- if (type == "mix")
+            "mix"  = function(t, phi, c) 1-c*exp(-t*phi),
+            "fmix"  = function(t, phi, c) 1-c*exp(-t*phi))
+    nll.fun <- if (type %in% c("mix", "fmix"))
         function(param) {
             if (length(param) < 2)
                 param <- c(param, 1)
-            CP <- pifun(D0, poisson("log")$linkinv(param[1]),
+            CP <- pifun(D0,
+                poisson("log")$linkinv(param[1]),
                 binomial("logit")$linkinv(param[2]))
             P <- CP - cbind(0, CP[, -k, drop=FALSE])
             Psum <- rowSums(P, na.rm=TRUE)
@@ -23,7 +25,8 @@ function(Y, D, type=c("rem", "mix", "dis"), interval=c(-25, 25), ...)
             if (nll %in% c(NA, NaN, Inf, -Inf))
                 nlimit[2] else nll
         } else function(param) {
-            CP <- pifun(D0, poisson("log")$linkinv(param))
+            CP <- pifun(D0,
+                poisson("log")$linkinv(param))
             P <- CP - cbind(0, CP[, -k, drop=FALSE])
             Psum <- rowSums(P, na.rm=TRUE)
             PPsum <- P / Psum
@@ -46,14 +49,14 @@ function(Y, D, type=c("rem", "mix", "dis"), interval=c(-25, 25), ...)
     Y0ok <- !is.na(Y0)
     n <- nrow(Y0)
     k <- ncol(Y0)
-    if (type != "mix") {
+    if (type %in% c("rem", "dis")) {
         out <- optimize(nll.fun, interval=interval, ...)
-        rval <- list(coef=out$minimum, 
+        rval <- list(coef=out$minimum,
             loglik=-out$objective)
     } else {
         out <- optimize(nll.fun, interval=interval, ...)
         out <- optim(c(out$minimum, 0), nll.fun, hessian=FALSE, ...)
-        rval <- list(coef=out$par, 
+        rval <- list(coef=out$par,
             loglik=-out$value)
     }
     rval
