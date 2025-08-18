8c64311624a2abc712e77c0090106df2ad91b627
Peter Solymos <psolymos@gmail.com>
2025-08-18 11:46:29 -0600

    Clean up species
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- data-raw/paired/paired.R
+++ data-raw/paired/paired.R
@@ -1,3 +1,47 @@
+x <- read.csv("data-raw/paired/aru-join-table_2016-AUG-22.csv")
+str(x)
+
+x$TimeInterval[x$TimeInterval == ""] <- "UNK"
+x$Interval[x$Interval == "0-1 min"] <- "0-3 min"
+x$Interval[x$Interval == "2-3 min"] <- "0-3 min"
+x$Interval[x$Interval == "8-9 min"] <- "5-10 min"
+table(x$TimeInterval,x$Interval)
+
+x$SurveyDate <- strptime(x$SurveyDate, "%d-%b-%y", tz = "Canada/Central")
+x$Time <- strptime(paste0(x$SurveyDate, " ", x$Time)[1], "%Y-%m-%d %I:%M:%S %p", tz = "Canada/Central")
+
+# WOSP = Woodpecker Spp = unidentified woodpecker = UNWO
+x$SPECIES[x$SPECIES == "WOSP"] <- "UNWO"
+# BADO = Barred Owl=BDOW
+x$SPECIES[x$SPECIES == "BADO"] <- "BDOW"
+# "CANG" Canada Goose=CAGO
+x$SPECIES[x$SPECIES == "CANG"] <- "CAGO"
+# "GRAJ" Canada Jay=CAJA
+x$SPECIES[x$SPECIES == "GRAJ"] <- "CAJA"
+# "YEWA" Yellow Warbler=YWAR
+x$SPECIES[x$SPECIES == "YEWA"] <- "YWAR"
+# "MYWA" Myrtle Warbler - considered Yellow-rumped Warbler=YRWA should we lump these 2?
+x$SPECIES[x$SPECIES == "MYWA"] <- "YRWA"
+# "UNBI" "UNBL" "UNDU" "UNHA" "UNKN" "UNSP" "UNWO" - will keep these as NA
+x$SPECIES[x$SPECIES %in% c("UNBI", "UNBL", "UNDU", "UNHA", "UNKN", "UNSP", "UNWO")] <- "UNKN"
+# "RESQ" Red Squirrel - not a bird
+# BCFR = Boreal Chorus Frog - not a bird
+# WOFR = Wood Frog - not a bird
+
+s <- read.csv("data-raw/paired/aou-codes.csv")
+s <- rbind(s,
+    data.frame(
+        id = c("UNKN", "RESQ", "BCFR", "WOFR"), 
+        name = c("Unknown", "Red Squirrel", "Boreal Chorus Frog", "Wood Frog")))
+sort(setdiff(unique(x$SPECIES), s$id))
+
+x$SpeciesName <- s$name[match(x$SPECIES, s$id)]
+
+colnames(x)[colnames(x) == "YEAR"] <- "FIREYEAR"
+paired <- x
+save(paired, file="data/paired.rda", compress = "xz")
+
+
 if (FALSE) {
 
 library(rJava)
@@ -23,41 +67,3 @@ zz$code_6_letter <- gsub("\\*", "", zz$code_6_letter)
 write.csv(zz, "data-raw/aou-alpha-codes-eng.csv", row.names = FALSE)
 
 }
-
-x <- read.csv("data-raw/paired/aru-join-table_2016-AUG-22.csv")
-str(x)
-
-x$TimeInterval[x$TimeInterval == ""] <- "UNK"
-x$Interval[x$Interval == "0-1 min"] <- "0-3 min"
-x$Interval[x$Interval == "2-3 min"] <- "0-3 min"
-x$Interval[x$Interval == "8-9 min"] <- "5-10 min"
-table(x$TimeInterval,x$Interval)
-
-x$SurveyDate <- strptime(x$SurveyDate, "%d-%b-%y", tz = "Canada/Central")
-x$Time <- strptime(paste0(x$SurveyDate, " ", x$Time)[1], "%Y-%m-%d %I:%M:%S %p", tz = "Canada/Central")
-
-s <- read.csv("data-raw/paired/aou-codes.csv")
-
-# "BADO" ???
-# "BCFR" ???
-# "WOFR" ???
-# "WOSP" ???
-# "CANG" Canada Goose=CAGO
-x$SPECIES[x$SPECIES == "CANG"] <- "CAGO"
-# "GRAJ" Canada Jay=CAJA
-x$SPECIES[x$SPECIES == "GRAJ"] <- "CAJA"
-# "YEWA" Yellow Warbler=YWAR
-x$SPECIES[x$SPECIES == "YEWA"] <- "YWAR"
-# "MYWA" Myrtle Warbler - considered Yellow-rumped Warbler=YRWA should we lump these 2?
-x$SPECIES[x$SPECIES == "MYWA"] <- "YRWA"
-# "UNBI" "UNBL" "UNDU" "UNHA" "UNKN" "UNSP" "UNWO" - will keep these as NA
-x$SPECIES[x$SPECIES %in% c("UNBI", "UNBL", "UNDU", "UNHA", "UNKN", "UNSP", "UNWO")] <- "UNKN"
-# "RESQ" Red Squirrel - not a bird
-
-sort(setdiff(unique(x$SPECIES), s$id))
-
-x$SpeciesName <- s$name[match(x$SPECIES, s$id)]
-
-colnames(x)[colnames(x) == "YEAR"] <- "FIREYEAR"
-paired <- x
-save(paired, file="data/paired.rda", compress = "xz")
